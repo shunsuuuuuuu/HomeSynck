d49d8fb60ac06a9d042e11b99d255ceeff7e5a2d
shunsuuuuuuu <chibichibihanabi@gmail.com>
2025-01-11 21:34:14 +0900

    動的に乗り換え情報を更新

--- public/js/search_transfer_info.js
+++ public/js/search_transfer_info.js
@@ -1,3 +1,5 @@
+import { displayFilteredProperties } from './property_list_mng.js';
+
 // 乗り換え情報を取得し、データベースを更新する関数
 export async function searchTransferInfo(properties, destStation) {
     for (const property of properties) {
@@ -7,6 +9,7 @@ export async function searchTransferInfo(properties, destStation) {
             const result = await fetchTransferInfo(id, address, destStation);
             if (result && result.success) {
                 console.log(`物件ID: ${id}, 目的駅: ${destStation} 乗り換え情報: ${result.data.ridetime}分, ${result.data.count}回`);
+                displayFilteredProperties();
             } else {
                 console.error(`物件ID: ${id}, エラー: ${result ? result.message : '不明なエラー'}`);
             }
@@ -18,9 +21,11 @@ export async function searchTransferInfo(properties, destStation) {
 
 async function fetchTransferInfo(id, address, destStation) {
     const response = await fetch(`/api/properties/get-transfer-info/${id}/${address}/${destStation}`);
-    if (!response.ok) {
+    if (response.ok) {
+        const result = await response.json();
+        return result;
+    }
+    else {
         throw new Error(`乗り換え情報の取得に失敗しました: ${response.statusText}`);
     }
-    const result = await response.json();
-    return result;
 }
